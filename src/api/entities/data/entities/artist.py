5e76ad5adbb4f285995a1d23fcb220652093fdbc
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-12-22 19:39:13 +0000

    Create Artist & GetAll Artist

---
+++ src/api/entities/data/entities/artist.py
@@ -0,0 +1,10 @@
+class Artist:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
---
+++ src/api/entities/data/entities/track.py
@@ -0,0 +1,9 @@
+class Track:
+    def __init__(self, id, title, url, streams, rank, id_artist, id_trend):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.streams = streams
+        self.rank = rank
+        self.id_artist = id_artist
+        self.id_trend = id_trend
