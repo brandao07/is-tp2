faeb7efbdcc18a2084b1dcc1452c28cf40bf9842
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-12-26 17:31:34 +0000

    ent frontend done

---
+++ src/frontend/ent/src/Tables/Artists.js
@@ -0,0 +1,95 @@
+import {useEffect, useState} from "react";
+import {
+    CircularProgress,
+    Pagination,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import axios from "axios";
+
+
+
+let artists = []
+
+
+function Artists() {
+    axios.get(`${process.env.REACT_APP_API_ENTITIES_URL}/api/artists/`).then((response) => {
+        console.log(response.data)
+        artists = response.data["Artists"]
+    }).catch((error) => {
+        console.log(error)
+    })
+    const PAGE_SIZE = 10;
+    const [page, setPage] = useState(1);
+    const [data, setData] = useState(null);
+    const [maxDataSize, setMaxDataSize] = useState(artists.length);
+
+    useEffect(() => {
+        setData(null);
+        setTimeout(() => {
+            console.log(`fetching from ${URL}`)
+            setData(artists.filter((item, index) => Math.floor(index / PAGE_SIZE) === (page - 1)));
+        }, 500);
+    }, [page])
+
+    return (
+        <>
+            <h1>Artists</h1>
+
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell component="th" align="center">ID</TableCell>
+                            <TableCell>Artist Name</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data ?
+                                data.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        style={{background: "gray", color: "black"}}
+                                    >
+                                        <TableCell component="td" align="center">{row.id}</TableCell>
+                                        <TableCell component="td" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                                :
+                                <TableRow>
+                                    <TableCell colSpan={3}>
+                                        <CircularProgress/>
+                                    </TableCell>
+                                </TableRow>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {
+                maxDataSize && <div style={{background: "black", padding: "1rem"}}>
+                    <Pagination style={{color: "black"}}
+                                variant="outlined" shape="rounded"
+                                color={"primary"}
+                                onChange={(e, v) => {
+                                    setPage(v)
+                                }}
+                                page={page}
+                                count={Math.ceil(maxDataSize / PAGE_SIZE)}
+                    />
+                </div>
+            }
+
+
+        </>
+    );
+}
+
+export default Artists;
---
+++ src/frontend/ent/src/Tables/Regions.js
@@ -0,0 +1,97 @@
+import {useEffect, useState} from "react";
+import {
+    CircularProgress,
+    Pagination,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import axios from "axios";
+
+let regions = []
+
+
+function Regions() {
+    axios.get(`${process.env.REACT_APP_API_ENTITIES_URL}/api//regions/`).then((response) => {
+        console.log(response.data)
+        regions = response.data["Regions"]
+    }).catch((error) => {
+        console.log(error)
+    })
+    const PAGE_SIZE = 10;
+    const [page, setPage] = useState(1);
+    const [data, setData] = useState(null);
+    const [maxDataSize, setMaxDataSize] = useState(regions.length);
+
+    useEffect(() => {
+        setData(null);
+        setTimeout(() => {
+            console.log(`fetching from ${process.env.REACT_APP_API_ENTITIES_URL}`)
+            setData(regions.filter((item, index) => Math.floor(index / PAGE_SIZE) === (page - 1)));
+        }, 500);
+    }, [page])
+
+    return (
+        <>
+            <h1>Regions</h1>
+
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell component="th" align="center">ID</TableCell>
+                            <TableCell>Region Name</TableCell>
+                            <TableCell align="center">Geometry</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            data ?
+                                data.map((row) => (
+                                    <TableRow
+                                        key={row.id}
+                                        style={{background: "gray", color: "black"}}
+                                    >
+                                        <TableCell component="td" align="center">{row.id}</TableCell>
+                                        <TableCell component="td" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell component="td" align="center" scope="row">
+                                            {row.geom}
+                                        </TableCell>
+                                    </TableRow>
+                                ))
+                                :
+                                <TableRow>
+                                    <TableCell colSpan={3}>
+                                        <CircularProgress/>
+                                    </TableCell>
+                                </TableRow>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {
+                maxDataSize && <div style={{background: "black", padding: "1rem"}}>
+                    <Pagination style={{color: "black"}}
+                                variant="outlined" shape="rounded"
+                                color={"primary"}
+                                onChange={(e, v) => {
+                                    setPage(v)
+                                }}
+                                page={page}
+                                count={Math.ceil(maxDataSize / PAGE_SIZE)}
+                    />
+                </div>
+            }
+
+
+        </>
+    );
+}
+
+export default Regions;
