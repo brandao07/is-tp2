72aa61d31a0fe231c7e03296599e54434ecbe32e
brandao07 <75815203+brandao07@users.noreply.github.com>
2022-12-26 15:53:19 +0000

    update feito

--- src/api/entities/data/entities/Artist.py
+++ src/api/entities/data/entities/Artist.py
@@ -1,15 +1,19 @@
-from data.db.db import get_all, insert, get_one, delete
+from data.db.db import get_all, execute, get_one
 from data.entities.EntityI import EntityI
 
 
 class Artist(EntityI):
     @staticmethod
     def delete(id):
-        return delete("update artists set is_deleted = true where id = %s", (id, ))
+        return execute("update artists set is_deleted = true where id = %s", (id, ))
 
     @staticmethod
-    def get_one(args):
-        return get_one("select id, name from artists where name like %s and is_deleted = false", (args[0],))
+    def get_one_by_name(name):
+        return get_one("select id, name from artists where name like %s and is_deleted = false", (name,))
+
+    @staticmethod
+    def get_by_id(id):
+        return get_one("select id, name from artists where id = %s and is_deleted = false", (id,))
 
     @staticmethod
     def get_all():
@@ -17,7 +21,14 @@ class Artist(EntityI):
 
     @staticmethod
     def insert(obj):
-        return insert('insert into artists (name) values (%s)', (obj.name, ))
+        return execute('insert into artists (name) values (%s)', (obj.name, ))
+
+    @staticmethod
+    def update(obj):
+        old = Artist.get_by_id(obj.id)
+        if obj.name == "":
+            obj.name = old[1]
+        return execute('update artists set name = %s where id = %s', (obj.name, obj.id))
 
     def __init__(self, id, name):
         self.id = id
--- src/api/entities/main.py
+++ src/api/entities/main.py
@@ -36,6 +36,15 @@ def delete_artist(id):
     return "error while deleting artist", 500
 
 
+@app.route('/api/artists/<id>', methods=['PUT'])
+def update_artist(id):
+    data = request.get_json()
+    if Artist.update(Artist(name=data['name'], id=id)):
+        return "artist updated successfully!", 201
+
+    return "error while updating artist", 500
+
+
 @app.route('/api/regions/', methods=['GET'])
 def get_regions():
     return jsonify({"Regions": [Serializable.region(x) for x in Region.get_all()]}), 200
@@ -51,12 +60,21 @@ def create_region():
     return "error while creating region", 500
 
 
-@app.route('/api/regions/', methods=['DELETE'])
+@app.route('/api/regions/<id>', methods=['DELETE'])
 def delete_region(id):
     if Region.delete(id):
         return "region deleted successfully!", 201
 
     return "error while deleting region", 500
+
+
+@app.route('/api/regions/<id>', methods=["PUT"])
+def update_region(id):
+    data = request.get_json()
+    if Region.update(Region(name=data['name'], id=id, geom=data['geom'], lat=None, lon=None)):
+        return "region updated successfully!", 201
+
+    return "error while updating region", 500
 
 
 @app.route('/api/tracks/', methods=['GET'])
@@ -74,20 +92,50 @@ def create_tracks():
                           date=data['date'],
                           rank=data['rank'],
                           trend=data['trend'],
-                          artists_id=Artist.get_one((data['artist_name'],))[0],
-                          regions_id=Region.get_one((data['region_name'],))[0],
+                          artists_id=Artist.get_one_by_name(data['artist_name'])[0],
+                          regions_id=Region.get_one_by_name(data['region_name'])[0],
                           id=None)):
         return "track created successfully!", 201
 
     return "error while creating track", 500
 
 
-@app.route('/api/tracks/', methods=['DELETE'])
+@app.route('/api/tracks/<id>', methods=['DELETE'])
 def delete_track(id):
     if Track.delete(id):
         return "track deleted successfully!", 201
 
     return "error while deleting track", 500
+
+
+@app.route('/api/tracks/<id>', methods=['PUT'])
+def update_track(id):
+    data = request.get_json()
+
+    if Artist.get_one_by_name(data['artist_name']) is None:
+        artists_id = ""
+    else:
+        artists_id = Artist.get_one_by_name(data['artist_name'])[0]
+
+    if Region.get_one_by_name(data['region_name']) is None:
+        regions_id = ""
+    else:
+        regions_id = Region.get_one_by_name(data['region_name'])[0]
+
+    if Track.update(Track(id=id,
+                          title=data['title'],
+                          trend=data['trend'],
+                          rank=data['rank'],
+                          url=data['url'],
+                          date=data['date'],
+                          streams=data['streams'],
+                          artists_id=artists_id,
+                          regions_id=regions_id
+                          )
+                    ):
+        return "track updated successfully!", 201
+
+    return "error while updating track", 500
 
 
 if __name__ == '__main__':
